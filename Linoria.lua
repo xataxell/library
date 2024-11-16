c02d9beb456743484e60f7b19ff70cc96a1f3a02
xataxell <felixargylesun@gmail.com>
2024-11-16 14:31:01 +0100

    Update Linoria.lua

--- Linoria.lua
+++ Linoria.lua
@@ -1379,14 +1379,14 @@ do
 
 	                for _, Label in next, Library.KeybindContainer:GetChildren() do
 		            if Label:IsA('TextLabel') and Label.Visible then
-			        YSize = YSize + Label.TextBounds.Y
+			        YSize = YSize + 18
 			        if (Label.TextBounds.X > XSize) then
 				    XSize = Label.TextBounds.X
 			        end
 		            end
 	                end
 
-	            Library.KeybindFrame.Size = UDim2.new(0, math.max(XSize + 10, 210), 0, YSize + 23)
+	            Library.KeybindFrame.Size = UDim2.new(0, math.max(XSize, 210), 0, YSize + 23)
                 end;
 
 
